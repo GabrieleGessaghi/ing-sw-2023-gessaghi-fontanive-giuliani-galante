2f3caad4f150886cc0e16c185ea5c24863add371
NiccoloGiuliani10710702 <niccolo.giuliani@mail.polimi.it>
2023-03-24 16:01:37 +0100

    FourGroups definition

---
+++ src/main/java/model/cards/concrteobjectives/FourGroups.java
@@ -0,0 +1,12 @@
+package model.cards.concrteobjectives;
+
+/**
+ * @author Niccolò Giuliani
+ * Four groups each containing at least
+ * 4 tiles of the same type (not necessarily
+ * in the depicted shape).
+ * The tiles of one group can be different
+ * from those of another group.
+ */
+public class FourGroups {
+}
