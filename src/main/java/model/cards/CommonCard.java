4f7fbe54f2847eed2e685257e5335d16f2d9603e
GurgleTheGreat <giorgio.fontanive@outlook.com>
2023-03-27 14:57:07 +0200

    Test

--- src/main/java/model/cards/CommonCard.java
+++ src/main/java/model/cards/CommonCard.java
@@ -8,68 +8,56 @@ import model.Token;
  common objective cards of the game
  */
 public class CommonCard extends Card {
-    private List<String> players;
+    private int numberOfTokensLeft;
     private int numberOfPlayers;
     private CommonObjective objective;
     private CommonType name;
 
     /**
      * Class constructor
-     * @author Niccolò Giuliani
-     * @param objective the algorithm for this card.
+     *
+     * @param objective       the algorithm for this card.
      * @param numberOfPlayers the number of players playing this game.
+     * @author Niccolò Giuliani
      */
     public CommonCard(CommonObjective objective, int numberOfPlayers) {
         this.objective = objective;
-        this.name=objective.getName();
-        this.numberOfPlayers=numberOfPlayers;
+        this.name = objective.getName();
+        this.numberOfPlayers = numberOfPlayers;
     }
 
     public int getPoints(Token[][] shelf) {
         boolean satisfied;
+        int points = 0;
         satisfied = objective.isSatisfied(shelf);
-        if(satisfied)
+        if (satisfied) {
             switch (numberOfPlayers) {
                 case 2:
-                    if(players.size() == 0)
+                    if (players.size() == 0)
                         return 8;
-                    else if(players.size() == 1)
+                    else if (players.size() == 1)
                         return 4;
                 case 3:
-                    if(players.size() == 0)
+                    if (players.size() == 0)
                         return 8;
-                    else if(players.size() == 1)
+                    else if (players.size() == 1)
                         return 6;
-                    else if(players.size() == 2)
+                    else if (players.size() == 2)
                         return 4;
                 case 4:
-                    if(players.size() == 0)
-                        return 8;
-                    else if(players.size() == 1)
-                        return 6;
-                    else if(players.size() == 2)
-                        return 4;
-                    else if(players.size() == 3)
-                        return 2;
+                    switch (numberOfTokensLeft) {
+                        case 4:
+                            points = 8;
+                        case 3:
+                            points = 6;
+                        case 2:
+                            points = 4;
+                        case 1:
+                            points = 2;
+                    }
             }
-        return 0;
-    }
-
-    /**
-     * Getter for the players list.
-     * @author Niccolò Giuliani
-     * @return the list of player names who have already taken the card.
-     */
-    public List getPlayers() {
-        return players;
-    }
-
-    /**
-     * Adds a player's name to the list.
-     * @author Niccolò Giuliani
-     * @param nickname the name chosen by the player.
-     */
-    public void addPlayer(String nickname){
-        players.add(nickname);
+            numberOfTokensLeft--;
+        }
+        return points;
     }
 }
