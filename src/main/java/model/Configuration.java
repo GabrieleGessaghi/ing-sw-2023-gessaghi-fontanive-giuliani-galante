efd617a606937b1e6a79c47d9dd9c9a087170c41
NiccoloGiuliani10710702 <niccolo.giuliani@mail.polimi.it>
2023-03-29 12:59:03 +0200

    added configuration files

---
+++ src/main/java/model/Configuration.java
@@ -0,0 +1,12 @@
+package model;
+/**
+ * configuration class
+ * @author Niccolò Giuliani
+*/
+public class Configuration {
+    public static int BOARD_SIZE,MAX_TOKENS_PER_TURN,ROWS_SHELF,TWOPLAYERS,THREEPLAYERS,FOURPLAYERS;
+    public static int[] TWOPLAYERS_COMMONCARD_POINTS;
+    public static int[] THREEPLAYERS_COMMONCARD_POINTS;
+    public static int[] FOURPLAYERS_COMMONCARD_POINTS;
+
+}
