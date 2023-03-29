f2167d08b763fddb5969d18a0bde4e88c2acc856
NiccoloGiuliani10710702 <niccolo.giuliani@mail.polimi.it>
2023-03-29 17:30:07 +0200

    DiagonalTest

--- src/test/java/model/cards/concreteobjectives/CornersTest.java
+++ src/test/java/model/cards/concreteobjectives/CornersTest.java
@@ -21,18 +21,38 @@ class CornersTest {
 
         Random randomType = new Random();
         type=Token.values()[randomType.nextInt(Token.values().length)];
+
+        for(int i = 0; i < ROWS_SHELF-1; i++){
+            for(int j = 0; j < COLUMNS_SHELF-1; j++) {
+                Random random = new Random();
+                shelf[i][j]=Token.values()[random.nextInt(Token.values().length)];
+            }
+        }
         shelf[0][0] = type;
         shelf[0][COLUMNS_SHELF-1] = type;
         shelf[ROWS_SHELF-1][0] = type;
         shelf[ROWS_SHELF-1][COLUMNS_SHELF-1] = type;
 
-        for(int i = 1; i < ROWS_SHELF-2; i++){
-            for(int j = 1; j < COLUMNS_SHELF-2; j++) {
+        assertEquals(true,firstCorner.isSatisfied(shelf));
+    }
+
+    void WrongScenario(){
+        var firstCorner = new Corners();
+        Token[][] shelf;
+        Token type;
+
+        shelf = new Token[ROWS_SHELF][COLUMNS_SHELF];
+
+
+        for(int i = 0; i < ROWS_SHELF; i++){
+            for(int j = 0; j < COLUMNS_SHELF; j++) {
                 Random random = new Random();
                 shelf[i][j]=Token.values()[random.nextInt(Token.values().length)];
             }
         }
-        assertEquals(true,firstCorner.isSatisfied(shelf));
+        shelf[0][0]=Token.CAT;
+        shelf[6][0]=Token.BOOK;
+        assertEquals(false,firstCorner.isSatisfied(shelf));
     }
 
 
