dd81b496640b747f074c29f8ae99ba07248b0ba3
GurgleTheGreat <giorgio.fontanive@outlook.com>
2023-03-30 14:43:10 +0200

    Fixed small issues

--- src/main/java/model/cards/concreteobjectives/FourGroups.java
+++ src/main/java/model/cards/concreteobjectives/FourGroups.java
@@ -16,18 +16,18 @@ public class FourGroups implements CommonObjective {
     @Override
     public boolean isSatisfied(Token[][] shelf) {
         int counter = 0;
-        boolean flag=false;
+        boolean flag;
         boolean [][] check;
         check = new boolean[ROWS][COLUMNS];
         for(int i = 0; i < ROWS ; i++)
-            for(int j = 0; j< COLUMNS ; j++)
+            for(int j = 0; j < COLUMNS ; j++)
                 check[i][j] = false;
         for (int i = 0; i < ROWS - 3 && counter < 6; i++){
             for (int j = 0; j < COLUMNS - 3 ; j++){
                 flag=false;
-                if(shelf[i][j] != Token.NOTHING && check[i][j]!= true) {
+                if(shelf[i][j] != Token.NOTHING && !check[i][j]) {
                     if (shelf[i][j] == shelf[i][j + 1] && shelf[i][j] == shelf[i][j + 2] && shelf[i][j] == shelf[i][j + 3] &&
-                            check[i][j + 1] == false && check[i][j + 2] == false && check[i][j + 3] == false){
+                            !check[i][j + 1] && !check[i][j + 2] && !check[i][j + 3]){
                         flag=true;
                         counter++;
                         check[i][j] = true;
@@ -36,8 +36,8 @@ public class FourGroups implements CommonObjective {
                         check[i][j + 3] = true;
                     }
 
-                    if (shelf[i][j] == shelf[i + 1][j] && shelf[i][j] == shelf[i + 2][j] && shelf[i][j] == shelf[i + 3][j] && flag == false  &&
-                            check[i + 1][j] == false && check[i + 2][j] == false && check[i + 3][j] == false ) {
+                    if (shelf[i][j] == shelf[i + 1][j] && shelf[i][j] == shelf[i + 2][j] && shelf[i][j] == shelf[i + 3][j] && !flag &&
+                            !check[i + 1][j] && !check[i + 2][j] && !check[i + 3][j]) {
                         counter++;
                         check[i][j] = true;
                         check[i + 1][j] = true;
@@ -47,10 +47,7 @@ public class FourGroups implements CommonObjective {
                 }
             }
         }
-        if(counter >= 4)
-            return true;
-        else
-            return false;
+        return counter >= 4;
     }
 
     public CommonType getName(){
