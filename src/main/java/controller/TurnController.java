b9223a64af74ac35c74a7ce0bc35884b73c1bb11
threedoorcoupe <giorgiomassimo.fontanive@mail.polimi.it>
2023-04-12 08:24:13 +0200

    Created TurnController

--- src/main/java/controller/TurnController.java
+++ src/main/java/controller/TurnController.java
@@ -4,6 +4,16 @@ package controller;
  *
  * @author
  */
-public class TurnController {
+public class TurnController implements Observer{
+    private int[][] selectedTiles;
+    private int selectedColumn;
 
+    public TurnController() {
+        selectedTiles = new int[1][1];
+        selectedColumn = -1;
+    }
+
+    public void update(Event event) {
+
+    }
 }
