8a58499ef3424e386f59344504e2f79919399657
threedoorcoupe <giorgiomassimo.fontanive@mail.polimi.it>
2023-04-12 08:34:55 +0200

    Added update method to TurnController

--- src/main/java/controller/TurnController.java
+++ src/main/java/controller/TurnController.java
@@ -1,5 +1,12 @@
 package controller;
 
+import com.google.gson.stream.JsonReader;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import static controller.Configurations.*;
+
 /**
  *
  * @author
@@ -9,11 +16,27 @@ public class TurnController implements Observer{
     private int selectedColumn;
 
     public TurnController() {
-        selectedTiles = new int[1][1];
+        selectedTiles = new int[SHELF_ROWS][SHELF_COLUMNS];
         selectedColumn = -1;
     }
 
     public void update(Event event) {
-
+        String jsonMessage = event.getJsonMessage();
+        String field;
+        JsonReader jsonReader;
+        try {
+            jsonReader = new JsonReader(new StringReader(jsonMessage));
+            jsonReader.beginObject();
+            while(jsonReader.hasNext()) {
+                field = jsonReader.nextName();
+                switch (field) {
+                    case "selectedTiles" -> selectedTiles = readMatrix(jsonReader);
+                    case "selectedColumn" -> selectedColumn = jsonReader.nextInt();
+                }
+            }
+            jsonReader.endObject();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
