5e15ebfd027393c1571b7fc3b6893a5323045d67
threedoorcoupe <giorgiomassimo.fontanive@mail.polimi.it>
2023-04-12 09:06:34 +0200

    Added checks for legal input in TurnController

--- src/main/java/controller/TurnController.java
+++ src/main/java/controller/TurnController.java
@@ -1,23 +1,54 @@
 package controller;
 
 import com.google.gson.stream.JsonReader;
+import model.Game;
 
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.Arrays;
 
 import static controller.Configurations.*;
 
 /**
- *
- * @author
+ * Receives data for a new turn
+ * @author Giorgio Massimo Fontanive
  */
 public class TurnController implements Observer{
     private int[][] selectedTiles;
     private int selectedColumn;
+    private final Game game;
 
-    public TurnController() {
+    public TurnController(Game game) {
         selectedTiles = new int[SHELF_ROWS][SHELF_COLUMNS];
         selectedColumn = -1;
+        this.game = game;
+    }
+
+    private boolean isMatrixLegal() {
+        boolean cellFlag;
+        boolean matrixFlag = true;
+        boolean[] cellValues = new boolean[MAX_TOKENS_PER_TURN + 1];
+        for (int i = 0; i < SHELF_ROWS && matrixFlag; i++)
+            for (int j = 0; j < SHELF_COLUMNS && matrixFlag; j++) {
+                Arrays.fill(cellValues, true);
+                cellFlag = false;
+                for (int k = 0; k < MAX_TOKENS_PER_TURN; k++)
+                    if (selectedTiles[i][j] != k - 1)
+                        cellValues[k] = false;
+                for (int k = 0; k < MAX_TOKENS_PER_TURN && !cellFlag; k++)
+                    if (cellValues[k])
+                        cellFlag = true;
+                matrixFlag = cellFlag;
+            }
+        return matrixFlag;
+    }
+
+    private boolean isColumnLegal() {
+        return selectedColumn >= 0 && selectedColumn <= SHELF_COLUMNS;
+    }
+
+    private boolean isTurnReady() {
+        return isMatrixLegal() && isColumnLegal();
     }
 
     public void update(Event event) {
@@ -38,5 +69,8 @@ public class TurnController implements Observer{
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        if (isTurnReady())
+            game.playerTurn(selectedTiles, selectedColumn);
     }
 }
