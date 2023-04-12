96ecddca62be34f3a6ffa0e18e9a4d2323f2e596
threedoorcoupe <giorgiomassimo.fontanive@mail.polimi.it>
2023-04-12 09:16:44 +0200

    Added documentantion

--- src/main/java/controller/TurnController.java
+++ src/main/java/controller/TurnController.java
@@ -5,12 +5,11 @@ import model.Game;
 
 import java.io.IOException;
 import java.io.StringReader;
-import java.util.Arrays;
 
 import static controller.Configurations.*;
 
 /**
- * Receives data for a new turn
+ * Receives data for a new turn and acts once it's ready.
  * @author Giorgio Massimo Fontanive
  */
 public class TurnController implements Observer{
@@ -24,33 +23,33 @@ public class TurnController implements Observer{
         this.game = game;
     }
 
+    /**
+     * Checks whether the saved matrix contains a valid number in each cell.
+     * @return True if the matrix represents a possible move.
+     */
     private boolean isMatrixLegal() {
-        boolean cellFlag;
         boolean matrixFlag = true;
-        boolean[] cellValues = new boolean[MAX_TOKENS_PER_TURN + 1];
         for (int i = 0; i < SHELF_ROWS && matrixFlag; i++)
-            for (int j = 0; j < SHELF_COLUMNS && matrixFlag; j++) {
-                Arrays.fill(cellValues, true);
-                cellFlag = false;
-                for (int k = 0; k < MAX_TOKENS_PER_TURN; k++)
-                    if (selectedTiles[i][j] != k - 1)
-                        cellValues[k] = false;
-                for (int k = 0; k < MAX_TOKENS_PER_TURN && !cellFlag; k++)
-                    if (cellValues[k])
-                        cellFlag = true;
-                matrixFlag = cellFlag;
-            }
+            for (int j = 0; j < SHELF_COLUMNS && matrixFlag; j++)
+                if (selectedTiles[i][j] < -1 || selectedTiles[i][j] > MAX_TOKENS_PER_TURN)
+                    matrixFlag = false;
         return matrixFlag;
     }
 
+    /**
+     * Checks whether the saved integer contains a valid column index.
+     * @author Giorgio Massimo Fontanive
+     * @return True if the integer represents a column.
+     */
     private boolean isColumnLegal() {
         return selectedColumn >= 0 && selectedColumn <= SHELF_COLUMNS;
     }
 
-    private boolean isTurnReady() {
-        return isMatrixLegal() && isColumnLegal();
-    }
-
+    /**
+     * Reads the JSON message and makes a new turn once all data is ready.
+     * @author Giorgio Massimo Fontanive
+     * @param event The event received from the observable object to which this is subscribed.
+     */
     public void update(Event event) {
         String jsonMessage = event.getJsonMessage();
         String field;
@@ -70,7 +69,8 @@ public class TurnController implements Observer{
             throw new RuntimeException(e);
         }
 
-        if (isTurnReady())
+        if (isMatrixLegal() && isColumnLegal())
             game.playerTurn(selectedTiles, selectedColumn);
+        //TODO: Maybe throw an exception
     }
 }
