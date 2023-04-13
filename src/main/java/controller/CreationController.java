6973f9952d07075346865d8d8f1afbfceb37311d
Niccolo Giuliani <niccolo.giuliani@mail.polimi.it>
2023-04-13 11:28:36 +0200

    added method to CreationController with javadoc

--- src/main/java/controller/CreationController.java
+++ src/main/java/controller/CreationController.java
@@ -1,9 +1,54 @@
 package controller;
 
-/**
- *
- * @author
- */
-public class CreationController {
+import model.Game;
 
+/**
+ * Controller of the creation of the game
+ * @author Niccolò Giuliani
+ */
+public class CreationController implements Observer {
+    private int playersNumber;
+    private String[] playersNicknames;
+    private boolean isGameReady;
+
+
+    @Override
+    public void update(Event event) {
+
+    }
+
+    /**
+     * getter of the state of the game (ready or not ready)
+     * @return true if game is ready
+     * @author Niccolò Giuliani
+     */
+    public boolean getIsGameReady(){
+        return true;
+    }
+
+    /**
+     * method to create the Game@return
+     * @return the Game created
+     * @author Niccolò Giuliani
+     */
+    public  Game createGame(){
+        return null;
+    }
+
+    /**
+     * set the number of the players
+     * @author Niccolò Giuliani
+     */
+    private void setPlayerNumber(){
+
+    }
+
+    /**
+     * method to add a player
+     * @param nickname nickname of the player to add
+     * @author Niccolò Giuliani
+     */
+    private void addPlayer(String nickname){
+
+    }
 }
