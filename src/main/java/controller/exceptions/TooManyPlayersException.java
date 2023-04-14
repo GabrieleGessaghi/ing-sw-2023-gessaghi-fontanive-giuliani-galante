a99d71d5c761ab11dd2f60dfb63bcdc14c8481a6
threedoorcoupe <giorgiomassimo.fontanive@mail.polimi.it>
2023-04-14 14:33:33 +0200

    Fixed small issues

--- src/main/java/controller/exceptions/TooManyPlayersException.java
+++ src/main/java/controller/exceptions/TooManyPlayersException.java
@@ -1,7 +1,6 @@
 package controller.exceptions;
 
 public class TooManyPlayersException extends Exception{
-
     public TooManyPlayersException (String errorMessage) {
         super(errorMessage);
     }
