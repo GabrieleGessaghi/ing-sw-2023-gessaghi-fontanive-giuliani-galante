f118cafec17e989404194ab320777f24b8a250ad
NicoGalaga <niccolo.galante@mail.polimi.it>
2023-04-18 11:27:03 +0200

    Tested TwoSquaresTest

---
+++ src/test/java/model/PlayerTest.java
@@ -0,0 +1,14 @@
+package model;
+
+import model.cards.CommonCard;
+import model.cards.PersonalCard;
+import org.junit.jupiter.api.Test;
+
+public class PlayerTest {
+    @Test
+    public void insertTokensTest(){
+        PersonalCard pc = new PersonalCard(1);
+        //List<CommonCard> commonCards =
+       // Player player = new Player("nick", false, pc, commonCards);
+    }
+}
