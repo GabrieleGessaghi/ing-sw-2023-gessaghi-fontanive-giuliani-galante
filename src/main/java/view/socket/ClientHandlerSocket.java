bba25d943eefe0735835e9ffe219396902ba2312
Niccolo Giuliani <niccolo.giuliani@mail.polimi.it>
2023-04-26 11:54:35 +0200

    fixed small issues

--- src/main/java/view/socket/ClientHandlerSocket.java
+++ src/main/java/view/socket/ClientHandlerSocket.java
@@ -5,18 +5,32 @@ import controller.observer.Observer;
 import view.ClientHandler;
 import view.Promt;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
 
 public class ClientHandlerSocket extends ClientHandler {
     private int port;
-    private InputStream inputstream;
-    private OutputStream outputstream;
+    private Promt lastPromt;
+    private InputStream inputStream;
+    private OutputStream outputStream;
+    private Socket s;
 
+    private ArrayList<Observer> observers;
+    public ClientHandlerSocket(ArrayList<Observer> observers, int port,Socket s, InputStream inputStream, OutputStream outputStream){
+        this.observers = observers;
+        this.port = port;
+        this.s = s;
+        this.inputStream = inputStream;
+        this.outputStream = outputStream;
 
+    }
     @Override
     public void updateObservers(Event event) {
-
+        for(Observer o : observers)
+            o.update(event);
     }
 
     @Override
@@ -29,19 +43,23 @@ public class ClientHandlerSocket extends ClientHandler {
 
     }
 
-    @Override
-    public void notifyObservers(Event event) {
 
-    }
 
     @Override
     public void requestInput(Promt promt) {
-
+        this.lastPromt = promt;
     }
 
     @Override
     public void run() {
+      /*  while(true){
+            try{
 
+
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }*/
     }
 
     @Override
