ac4db5f4b66e139188a7706f0f64a29d63c87e7a
threedoorcoupe <giorgiomassimo.fontanive@mail.polimi.it>
2023-04-27 14:17:25 +0200

    Updated Controller

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,12 +1,17 @@
 package controller;
 
+import com.google.gson.stream.JsonReader;
 import controller.observer.Event;
 import controller.observer.Observer;
 import model.Game;
 import view.socket.ClientHandlerSocket;
 
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Map;
+
+import static controller.Configurations.readMatrix;
 
 /**
  * Handles the game flow on a different thread.
@@ -36,8 +41,22 @@ public class Controller extends Thread implements Observer {
 
     @Override
     public void update(Event event) {
-
-        //PARSE JSON
+        String jsonMessage = event.getJsonMessage();
+        String field;
+        JsonReader jsonReader;
+        try {
+            jsonReader = new JsonReader(new StringReader(jsonMessage));
+            jsonReader.beginObject();
+            while(jsonReader.hasNext()) {
+                field = jsonReader.nextName();
+                switch (field) {
+                    //PARSE JSON
+                }
+            }
+            jsonReader.endObject();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         if (!isGameRunning && creationController.isGameReady()) {
             Game game = creationController.createGame();
--- src/main/java/controller/Server.java
+++ src/main/java/controller/Server.java
@@ -14,9 +14,12 @@ import java.net.Socket;
  */
 public class Server {
     private static Controller controller;
+    private static int connectionsCount;
     public static void main() throws IOException {
+        //TODO: Load configurations
         controller = new Controller();
         controller.start();
+        connectionsCount = 0;
         while (true) {
             Socket socket = null;
             try {
@@ -25,10 +28,12 @@ public class Server {
                 socket = serverSocket.accept();
                 DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
                 DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-                ClientHandlerSocket clientHandler = new ClientHandlerSocket(0, socket, dataInputStream, dataOutputStream);
+                ClientHandlerSocket clientHandler = new ClientHandlerSocket(connectionsCount, socket, dataInputStream, dataOutputStream);
                 clientHandler.start();
                 controller.addClient(clientHandler);
+                connectionsCount++;
                 //TODO: Register observers
+                //TODO: Lower connections count
             } catch (Exception e) {
                 if (socket != null)
                     socket.close();
