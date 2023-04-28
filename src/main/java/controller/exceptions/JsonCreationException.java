64c4fb56fd7ea02c9c0e53e9d6870d2389072d7b
threedoorcoupe <giorgiomassimo.fontanive@mail.polimi.it>
2023-04-28 17:40:45 +0200

    Finished JsonTools

---
+++ src/main/java/controller/exceptions/JsonCreationException.java
@@ -0,0 +1,7 @@
+package controller.exceptions;
+
+public class JsonCreationException extends Exception{
+    public JsonCreationException(String errorMessage) {
+        super(errorMessage);
+    }
+}
--- src/main/java/controller/utilities/JsonTools.java
+++ src/main/java/controller/utilities/JsonTools.java
@@ -1,35 +1,66 @@
 package controller.utilities;
 
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import controller.exceptions.JsonCreationException;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class JsonTools {
 
-//    private void addProperty(JsonObject jsonObject, String field, Object property) {
-//        if (property.getClass().equals(Integer.class))
-//            jsonObject.addProperty(field, (Integer) property);
-//        else if (property.getClass().equals(String.class))
-//            jsonObject.addProperty(field, (String) property);
-//        else if (property.getClass().equals(ArrayList.class)) {
-//            ArrayList<Integer> list = (ArrayList<Integer>) property;
-//            int[] array = list.stream().mapToInt(x -> x).toArray();
-//            jsonObject.add(field, createJsonArray(array));
-//        }
-//    }
-
-    public static String createJson(Map<String, Object> elements) {
-        return null;
+    private static void addProperty(JsonObject jsonObject, String field, Object property) throws JsonCreationException {
+        if (property.getClass().equals(Integer.class))
+            jsonObject.addProperty(field, (Integer) property);
+        else if (property.getClass().equals(String.class))
+            jsonObject.addProperty(field, (String) property);
+        else if (property.getClass().equals(JsonArray.class))
+            jsonObject.add(field, (JsonArray) property);
+        else throw new JsonCreationException("Unknows property class!");
     }
 
-    public static Map<String, Object> parseJson(String jsonMessage) {
-        return null;
+    public static String createJson(Map<String, Object> elements) {
+        JsonObject jsonObject = new JsonObject();
+        for (String field : elements.keySet()) {
+            try {
+                addProperty(jsonObject, field, elements.get(field));
+            } catch (JsonCreationException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return jsonObject.toString();
+    }
+
+    public static HashMap<String, Object> parseJson(String jsonMessage) {
+        HashMap<String, Object> elements = new HashMap<>();
+        try {
+            String field;
+            JsonReader jsonReader = new JsonReader(new StringReader(jsonMessage));
+            jsonReader.beginObject();
+            while(jsonReader.hasNext()) {
+                field = jsonReader.nextName();
+                switch (jsonReader.peek()) {
+                    case NUMBER -> elements.put(field, jsonReader.nextInt());
+                    case STRING -> elements.put(field, jsonReader.nextString());
+                    case BEGIN_ARRAY -> {
+                        if (jsonReader.peek() == JsonToken.BEGIN_ARRAY)
+                            elements.put(field, readMatrix(jsonReader));
+                        else
+                            elements.put(field, readArray(jsonReader));
+                    }
+                }
+            }
+            jsonReader.endObject();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return elements;
     }
 
     public static JsonArray createJsonArray(int[] array) {
