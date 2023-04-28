4d7081a9943d32369a396570c411129933dc0d59
Niccolo Giuliani <niccolo.giuliani@mail.polimi.it>
2023-04-28 15:58:34 +0200

    grouped code

--- src/main/java/model/cards/concreteobjectives/ThreeColumns.java
+++ src/main/java/model/cards/concreteobjectives/ThreeColumns.java
@@ -13,31 +13,8 @@ import model.cards.ModelUtil;
 public class ThreeColumns implements CommonObjective {
     @Override
     public boolean isSatisfied(Token[][] shelf) {
-        int[] counter = new int[7];
-        int differentType;
-        int atLeastThree = 0;
-        for (int i = 0; i < COLUMNS; i++) {
-            for (int w=0; w < 7 ; w++)
-                counter[w]=0;
-            differentType = 0;
-            for (int j=0; j < ROWS ; j++)
-                switch (shelf[j][i]) {
-                    case CAT -> counter[0]++;
-                    case BOOK -> counter[1]++;
-                    case TOY -> counter[2]++;
-                    case TROPHY -> counter[3]++;
-                    case FRAME -> counter[4]++;
-                    case PLANT -> counter[5]++;
-                    case NOTHING -> counter[6]++;
-                }
-            for (int n = 0; n < 6; n++)
-                if (counter[n] > 0)
-                    differentType++;
-            if (differentType <= 3 && counter[6] == 0)
-                atLeastThree++;
-        }
         ModelUtil util = new ModelUtil();
-        return atLeastThree >= 3;
+        return util.columnsChecker(shelf,false) >= 3;
     }
 
     public CommonType getName(){
