d0e7872b94565a5f4bb2cc955d4a4c3d4b081555
GabrieleGessaghi <gabrielegessaghi@gmail.com>
2023-05-08 18:54:58 +0200

    javaFX gui setup

---
+++ src/main/java/GUI/ClientGUI.java
@@ -0,0 +1,17 @@
+package GUI;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ClientGUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("MyShelfie");
+        primaryStage.show();
+    }
+}
