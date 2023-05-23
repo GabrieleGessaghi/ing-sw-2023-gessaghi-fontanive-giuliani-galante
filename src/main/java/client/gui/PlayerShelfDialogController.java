ef296162b81c54a82d4aa8c3f9fb1cab051dee62
GabrieleGessaghi <gabrielegessaghi@gmail.com>
2023-05-23 10:35:09 +0200

    player shelf dialog completed

--- src/main/java/client/gui/PlayerShelfDialogController.java
+++ src/main/java/client/gui/PlayerShelfDialogController.java
@@ -1,7 +1,9 @@
 package client.gui;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -20,44 +22,36 @@ public class PlayerShelfDialogController {
     public void initialize(){}
 
     @FXML
-    public void initShelf(int [][] tokens){
-        for(int i = 0; i < shelfGrid.getRowCount(); i++){
+    public void initShelf(int [][] tokens) {
+        for (int i = 0; i < shelfGrid.getRowCount(); i++) {
             for(int j = 0; j < shelfGrid.getColumnCount(); j++) {
-                StackPane stackPane = new StackPane();
-                stackPane.setPrefWidth(49);
-                stackPane.setPrefHeight(46);
-                ImageView imageView = new ImageView();
-                imageView.setFitWidth(49);
-                imageView.setFitHeight(46);
-                imageView.setImage(new Image("assets/item_tiles/Gatti1.1.png"));
                 Random rng = new Random();
                 int pictureNumber = rng.nextInt(3) + 1;
-                stackPane.getChildren().add(imageView);
-                shelfGrid.add(stackPane, i, j);
-//                switch (tokens[i][j]) {
-//                    case 0 -> {
-//                        imageView.setImage(new Image("assets/item_tiles/Gatti1.1.png"));
-//                    }
-//                    case 1 -> {
-//                        imageView.getStyleClass().add("cat_image" + pictureNumber);
-//                    }
-//                    case 2 -> {
-//                        imageView.getStyleClass().add("book_image" + pictureNumber);
-//                    }
-//                    case 3 -> {
-//                        imageView.getStyleClass().add("toy_image" + pictureNumber);
-//                    }
-//                    case 4 -> {
-//                        imageView.getStyleClass().add("trophy_image" + pictureNumber);
-//                    }
-//                    case 5 -> {
-//                        imageView.getStyleClass().add("frame_image" + pictureNumber);
-//                    }
-//                    case 6 -> {
-//                        imageView.getStyleClass().add("plant_image" + pictureNumber);
-//                        imageView.setOpacity(1);
-//                    }
-//                }
+                ImageView imageView = (ImageView) getNodeByRowColumnIndex(i,j);
+                switch (tokens[i][j]) {
+                    case 0 -> {
+                        imageView.setOpacity(0);
+                    }
+                    case 1 -> {
+                        imageView.getStyleClass().add("cat_image" + pictureNumber);
+                    }
+                    case 2 -> {
+                        imageView.getStyleClass().add("book_image" + pictureNumber);
+                    }
+                    case 3 -> {
+                        imageView.getStyleClass().add("toy_image" + pictureNumber);
+                    }
+                    case 4 -> {
+                        imageView.getStyleClass().add("trophy_image" + pictureNumber);
+                    }
+                    case 5 -> {
+                        imageView.getStyleClass().add("frame_image" + pictureNumber);
+                    }
+                    case 6 -> {
+                        imageView.getStyleClass().add("plant_image" + pictureNumber);
+                        imageView.setOpacity(1);
+                    }
+                }
             }
         }
     }
@@ -65,6 +59,32 @@ public class PlayerShelfDialogController {
     void reloadShelf(ActionEvent event) {
 
     }
+    private Node getNodeByRowColumnIndex (int row,int column) {
+        Node result = null;
+        int gridRow;
+        int gridColumn;
+        ObservableList<Node> children = shelfGrid.getChildren();
+        for (Node node : children) {
+            try {
+                if(GridPane.getRowIndex(node)  == null)
+                    gridRow = 0;
+                else
+                    gridRow = GridPane.getRowIndex(node);
 
+                if(GridPane.getColumnIndex(node)  == null)
+                    gridColumn = 0;
+                else
+                    gridColumn = GridPane.getColumnIndex(node);
+
+                if (gridRow == row && gridColumn == column) {
+                    result = node;
+                    break;
+                }
+            } catch(NullPointerException ex){
+                throw new RuntimeException(ex);
+            }
+        }
+        return result;
+    }
 }
 
